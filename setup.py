b3251685109176a0012fd67a3bc9f89f40057386
Tantowi JH <tjmailone@gmail.com>
2023-06-18 23:35:57 +0700

    update

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='simpleplayer',
-    version='1.0.1',
+    version='1.0.2',
     description='Module for playing audio files using soundfile and sounddevice.',
     long_description=long_description,
     long_description_content_type='text/markdown',
--- simpleplayer/simpleplayer.py
+++ simpleplayer/simpleplayer.py
@@ -4,6 +4,7 @@ Module for playing audio files using soundfile and sounddevice.
 
 import soundfile as sf
 import sounddevice as sd
+import time
 
 
 class AudioPlayer:
@@ -13,14 +14,18 @@ class AudioPlayer:
         self.stream = None
         self.paused = False
         self.current_index = 0
+        info = sf.info(filename)
+        self.duration = info.duration
 
     def play(self) -> None:
+        channels = 1 if self.data.ndim == 1 else self.data.shape[1]
         self.stream = sd.OutputStream(
             samplerate=self.sample_rate,
-            channels=self.data.shape[1],
+            channels=channels,
             callback=self._audio_callback
         )
         self.stream.start()
+
 
     def _audio_callback(self, outdata, frames, time, status):
         if self.paused:
@@ -28,11 +33,19 @@ class AudioPlayer:
         else:
             remaining_frames = len(self.data) - self.current_index
             if remaining_frames <= frames:
-                outdata[:remaining_frames, :] = self.data[self.current_index:self.current_index+remaining_frames, :]
+                if self.data.ndim == 1:
+                    self.data = self.data.reshape(-1, 1)  # Reshape to (n, 1)
+                    outdata[:remaining_frames, :] = self.data[self.current_index:self.current_index+remaining_frames, :]
+                else:
+                    outdata[:remaining_frames, :] = self.data[self.current_index:self.current_index+remaining_frames, :]
                 outdata[remaining_frames:, :] = 0
                 self.stop()
             else:
-                outdata[:frames, :] = self.data[self.current_index:self.current_index+frames, :]
+                if self.data.ndim == 1:
+                    self.data = self.data.reshape(-1, 1)  # Reshape to (n, 1)
+                    outdata[:frames, :] = self.data[self.current_index:self.current_index+frames, :]
+                else:
+                    outdata[:frames, :] = self.data[self.current_index:self.current_index+frames, :]
                 self.current_index += frames
 
     def pause(self) -> None:
@@ -48,3 +61,7 @@ class AudioPlayer:
             self.stream = None
             self.current_index = 0
             self.paused = False
+
+    def wait(self) -> None:
+        seconds = int(self.duration)
+        time.sleep(seconds)
